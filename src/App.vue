1cc2bcd1e6742d09ba78f3ba858a6418f91e7ce8
Vue Play <author@vueplay.com>
2023-09-10 01:22:56 +0200

    Stored by Vue Play

--- src/App.vue
+++ src/App.vue
@@ -44,22 +44,6 @@
 				fill="currentColor"
 				class="w-5 h-5 mr-1 mb-1"
 			>
-				<path
-					fill-rule="evenodd"
-					d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
-					clip-rule="evenodd"
-				/>
-			</svg><router-link to="/contact">
-				Contact
-			</router-link>
-		</li>
-		<li>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 24 24"
-				fill="currentColor"
-				class="w-5 h-5 mr-1 mb-1"
-			>
 				<path d="M11.584 2.376a.75.75 0 01.832 0l9 6a.75.75 0 11-.832 1.248L12 3.901 3.416 9.624a.75.75 0 01-.832-1.248l9-6z" />
 				<path
 					fill-rule="evenodd"
@@ -98,7 +82,9 @@
 			</ul>
 		</li>
 	</TopMenu>
-	<div class=""><router-view class="" /></div>
+	<div class="">
+		<router-view class="" />
+	</div>
 </template>
 <script>
 	import TopMenu from '@/vueplay/TopMenu.vue';
