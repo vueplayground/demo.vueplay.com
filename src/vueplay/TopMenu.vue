7fb65deb8538b42e4d47dced97d80081eb804792
Vue Play <author@vueplay.com>
2023-09-09 23:54:11 +0200

    Stored by Vue Play

--- src/vueplay/TopMenu.vue
+++ src/vueplay/TopMenu.vue
@@ -6,7 +6,7 @@
 
 		<div class="vp-logo">
 			<slot name="logo">
-				<a href="#" style="display: inline-flex">
+				<router-view to="/" style="display: inline-flex">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="orange" class="ml-4 w-12">
 						<path fill-rule="evenodd"
 							d="M12.963 2.286a.75.75 0 00-1.071-.136 9.742 9.742 0 00-3.539 6.177A7.547 7.547 0 016.648 6.61a.75.75 0 00-1.152-.082A9 9 0 1015.68 4.534a7.46 7.46 0 01-2.717-2.248zM15.75 14.25a3.75 3.75 0 11-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 011.925-3.545 3.75 3.75 0 013.255 3.717z"
@@ -15,7 +15,7 @@
 					<div class="text-2xl ml-3" style="display: inline-flex; align-items: center">
 						logo
 					</div>
-				</a>
+				</router-view>
 			</slot>
 		</div>
 
@@ -163,6 +163,7 @@ export default {
 	display: inline-flex;
 	height: 100%;
 	align-items: center;
+	cursor: pointer;
 }
 
 .vp-topmenu menu {
